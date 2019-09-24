d32692d1fcb43de330e37c7e8cf27e8083d8ca4d
cristiano-degiorgis <cri@webprofessor.it>
2019-09-24 23:52:00 +0200

    travis

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,10 @@
 # find public -type f -exec curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://ftp.webprofessor.it/web/{} ";"
 #find public -type f -exec curl -v -u $FTP_USER:$FTP_PASSWORD --use-ascii --ftp-create-dirs -T {} ftp://ftp.webprofessor.it/web/{} \;
 
+set -e
+
+sudo apt-get install -y lftp
+
 LOCALPATH='./public'
 REMOTEPATH='/web/public'
 
