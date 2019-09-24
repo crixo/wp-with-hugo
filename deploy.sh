45f8b1f3896dc47625f766547538b42cea2614e7
cristiano-degiorgis <cri@webprofessor.it>
2019-09-25 00:46:07 +0200

    lftp

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,12 @@ REMOTEPATH='/web/public/'
 # set ssl:verify-certificate no
 # set ftp:ssl-allow no
 
-lftp -f "
-open ftp://ftp.webprofessor.it
-user $FTP_USER $FTP_PASSWORD
-set ftp:ssl-allow no
-mirror --continue --reverse --delete --verbose $LOCALPATH $REMOTEPATH
-bye
-"
+# lftp -f "
+# open ftp://ftp.webprofessor.it
+# user $FTP_USER $FTP_PASSWORD
+# set ftp:ssl-allow no
+# mirror --continue --reverse --delete --verbose $LOCALPATH $REMOTEPATH
+# bye
+# "
+lftp -e "set ftp:ssl-allow no; mirror -R /public/ web/" -u $FTP_USER,$FTP_PASSWORD ftp.webprofessor.it
+
