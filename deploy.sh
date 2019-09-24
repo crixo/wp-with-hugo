fa43dd9fcbfd4ac796263181efea3b30855000cd
cristiano-degiorgis <cri@webprofessor.it>
2019-09-24 23:49:17 +0200

    travis

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 # find public -type f -exec curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://ftp.webprofessor.it/web/{} ";"
-find public -type f -exec curl -v -u $FTP_USER:$FTP_PASSWORD --use-ascii --ftp-create-dirs -T {} ftp://ftp.webprofessor.it/web/{} \;
+#find public -type f -exec curl -v -u $FTP_USER:$FTP_PASSWORD --use-ascii --ftp-create-dirs -T {} ftp://ftp.webprofessor.it/web/{} \;
 
+LOCALPATH='./public'
+REMOTEPATH='/web/public'
+
+lftp -f "
+open ftp://ftp.webprofessor.it
+user $FTP_USER $FTP_PASSWORD
+mirror --continue --reverse --delete $LOCALPATH $REMOTEPATH
+bye
+"
