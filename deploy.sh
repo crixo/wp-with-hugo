7bb8cd2cfa996784c600e6165a582f80184146ba
cristiano-degiorgis <cri@webprofessor.it>
2019-09-24 22:50:45 +0200

    travis

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find public -type f -exec curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://ftp.webprofessor.it/web/
+find public -type f -exec curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://ftp.webprofessor.it/web/ ";"
